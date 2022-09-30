64ce6384435b36464e1ed8e213f5b2722dd0874d
Bulmang <hmk113737@gmail.com>
2022-09-30 11:37:08 +0900

    Initial Commit

---
+++ AnimatedTabIcon/AnimatedTabIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTabIconApp.swift
+//  AnimatedTabIcon
+//
+//  Created by 하명관 on 2022/09/30.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTabIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
